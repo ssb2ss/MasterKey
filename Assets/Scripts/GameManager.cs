1747188856a28efa84cc2168c990aee8b8050424
ssb2ss <44504789+ssb2ss@users.noreply.github.com>
2019-07-23 16:49:23 +0900

    1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,27 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
-    int score;
+    public Sprite[] keySpirte;
+
+    private int score;
+    private int[] currentKeyShape;
+
+    private void Awake()
+    {
+        currentKeyShape = new int[8];
+    }
+
+    public void SetKeyShape()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            currentKeyShape[i] = Random.Range(0, 4);
+        }
+    }
+
+    public void OnKeySelected(int index)
+    {
+        Debug.Log(index);
+    }
 
 }
